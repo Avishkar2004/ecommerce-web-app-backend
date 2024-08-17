e1f003628bd18b59642cd3fd7eecc81e2310c4f2
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-08-17 16:01:32 +0530

    Post Flat

--- controllers/flatController.js
+++ controllers/flatController.js
@@ -6,10 +6,9 @@ const getAllFlats = (req, res) => {
       return res.status(500).json({ error: 'Database query error' });
     }
 
-    // Process each flat
     const flats = results.map(flat => ({
       ...flat,
-      photos: flat.photos ? `data:image/jpeg;base64,${flat.photos.toString('base64')}` : ''
+      photos: flat.photos ? `data:image/jpeg;base64,${Buffer.from(flat.photos).toString('base64')}` : '' // Convert binary data to base64
     }));
 
     res.json(flats);
